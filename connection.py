4473fc73fd35dea7820f623a6d43bd3024346cb9
George Souza Farias <georgesouzafarias@gmail.com>
2019-03-26 21:46:17 -0300

    Create a getter currency historical

--- connection.py
+++ connection.py
@@ -19,5 +19,24 @@ def get_stocks():
     print(json_data)
 
 
+def get_currency():
+    API = "70CCIQQ5ZMGX1Y1O"
+    main_api = "https://www.alphavantage.co/query?"
+    period = 'CURRENCY_EXCHANGE_RATE'
+    from_currency = 'USD'
+    to_currency = 'BRL'
+    api_key = '70CCIQQ5ZMGX1Y1O'
+
+    url = main_api + urllib.parse.urlencode({"function": period}) + "&" + urllib.parse.urlencode(
+        {"from_currency": from_currency}) + "&" + urllib.parse.urlencode(
+        {"to_currency": to_currency}) + "&" + urllib.parse.urlencode(
+        {"apikey": api_key})
+
+    json_data = requests.get(url).json()
+    print(url)
+    print(json_data)
+
+
 if __name__ == '__main__':
     get_stocks()
+    get_currency()

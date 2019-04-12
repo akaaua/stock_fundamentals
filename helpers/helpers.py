9ce543e0d2207c26ffeef83652b19fd634930ece
George Souza Farias <georgesouzafarias@gmail.com>
2019-04-11 22:04:53 -0300

    refactoring the class main

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,3 +1,5 @@
+from models.company import Company
+
 def apresentation():
     print("**************************************")
     print("*** Wellcome to Stock Fundamentals ***")
@@ -7,3 +9,18 @@ def apresentation():
 def ask_user():
     company_symbol = input('\n Please, insert the company symbol: ')
     return company_symbol
+
+def isValid(stock_data):
+
+    if (stock_data[ 1 ] is 0):
+        return True
+    else:
+        return False
+
+def extract_values(stock_data):
+    stock_metadata = stock_data[0][ 'Meta Data' ]
+    company_symbol = stock_metadata[ '2. Symbol' ]
+    company = Company(company_symbol)
+    stock_historical = stock_data[0][ 'Time Series (Daily)' ]
+    company.add_records(stock_historical)
+    return company
--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 from dao.connection import get_stocks
-from models.company import Company
-from helpers.helpers import apresentation,ask_user
+from helpers.helpers import apresentation,ask_user,isValid,extract_values
 
 
 def play():
     apresentation()
+
     carry_on=True
 
     while carry_on:
+
         stock_data = get_stocks(ask_user())
 
-        if (stock_data[1] is 0):
-            stock_metadata = stock_data[0][ 'Meta Data' ]
-            company_symbol = stock_metadata[ '2. Symbol' ]
-            company = Company(company_symbol)
-
-            stock_historical = stock_data[0][ 'Time Series (Daily)' ]
-            company.add_records(stock_historical)
+        if (isValid(stock_data)):
+            company = extract_values(stock_data)
+            print('Company with Symbol: {}'.format(company.symbol))
+            print('Total of record is: {}'.format(company.count_records))
+            print('List of records are: {}'.format(company.records))
         else:
             continue
 
 if __name__ == '__main__':
    play()
-
-
-
-
-
